5d4a9660058e3c206b049cae1859bddb0caa7ba3
Mariya643 <mariyanichiporuk643@gmail.com>
2024-10-31 13:35:22 +0300

    Добавлена проверка корректности номера с помощью регулярных выражений

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import requests
+import re
 
-user = ""
-password = ""
-sender = ""
+def validate_phone_number(phone_number):
+    pattern = r"^\d{11}$"
+    return bool(re.match(pattern, phone_number))
+
+user = "Mariya643"
+password = "818385"
+sender = "Mariya"
 receiver = "79997358253"
 text = "Hello, world!"
 
-url = f"https://my3.webcom.mobi/sendsms.php?user={user}&pwd={password}&sadr={sender}&dadr={receiver}&text={text}"
-print(url)
-response = request.get(url)
-print(response)
-if response.status_code == 200:
-    print("Сообщение успешно отправлено")
+if not validate_phone_number(receiver):
+    print("Ошибка неккоректный номер телефона")
 else:
-    print(f"ошибка при отправке {response.status_code}")
+    url = f"https://my3.webcom.mobi/sendsms.php?user={user}&pwd={password}&sadr={sender}&dadr={receiver}&text={text}"
+    print(url)
+    try:
+        response = requests.get(url)
+        print(response)
+        if response.status_code == 200:
+            print("Сообщение успешно отправлено")
+        else:
+            print(f"ошибка при отправке {response.status_code}")
+    except Exception as e:
+        print(f"Непредвиденная ошибка с кодом {e}")
